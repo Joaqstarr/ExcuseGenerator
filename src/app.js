b47a4c3cf21eb2352c7499058c480cd486a712e7
Joaquin Royer <73140474+Joaqstarr@users.noreply.github.com>
2024-06-01 00:12:16 +0000

    excuse generator finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let text = document.getElementById("excuse");
+
+function GenerateExcuse() {
+  let excuse = `${GetRandomItem(who)} ${GetRandomItem(action)} ${GetRandomItem(
+    what
+  )} ${GetRandomItem(when)}`;
+
+  return excuse;
+}
+
+function GetRandomItem(array) {
+  const index = Math.ceil(Math.random() * array.length) - 1;
+  return array[index];
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  text.innerText = GenerateExcuse();
 };
